4e35cf68137ddcb060485804bed4e86ba167695a
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:30:26 +0530

    Update intersectionArray.java

--- intersectionArray.java
+++ intersectionArray.java
@@ -1,5 +1,15 @@
 package lecture1;
 
 public class intersectionArray {
+for (int i = 0; i < arr1.length; i++) {
 
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i]==arr2[j]){
+                    System.out.print(arr1[i]+" ");
+                    arr2[j]=-9999999;
+                    break;
+                }
+            }
+        }
+    }
 }
