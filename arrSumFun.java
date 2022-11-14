546b6b2a6d1eccdd23dff47cc1f09edaa6e9e5c2
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:23:33 +0530

    Update arrSumFun.java

--- arrSumFun.java
+++ arrSumFun.java
@@ -1,5 +1,15 @@
 package lecture1;
 
 public class arrSumFun {
-
+	
+	public static int sum(int[] arr) {
+	 
+	  
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            
+            sum +=arr[i];
+        }
+        return sum;
+    }
 }
