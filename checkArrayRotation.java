ccbb9a76fab3783f21cdfa98b18364c135bef6e9
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:35:30 +0530

    Update checkArrayRotation.java

--- checkArrayRotation.java
+++ checkArrayRotation.java
@@ -2,8 +2,16 @@ package lecture1;
 
 public class checkArrayRotation {
 
-	public checkArrayRotation() {
-		// TODO Auto-generated constructor stub
-	}
-
+	   int count=1;
+        if(arr.length<=1){
+            return 0;
+        }
+        
+    	 for(int i=0;i<arr.length-1;i++)
+        {
+            if(arr[i]>arr[i+1])
+                return i+1;
+        }
+        return 0;
+    }
 }
