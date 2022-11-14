7b0f55ac0a8f23e47b1764a441d7825b25ac8023
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:39:44 +0530

    Update insertion.java

--- insertion.java
+++ insertion.java
@@ -1,5 +1,13 @@
 package lecture1;
 
 public class insertion {
-
+  for(int i=1; i<arr.length ; i++){
+            int current =arr[i];
+            int j;
+            for(j=i-1; j>=0&&current<arr[j];j--){
+                   arr[j+1]=arr[j];
+            }
+            arr[j+1]=current;
+        }
+    }
 }
