0dbc0f2086a093d4f83228143bef8a620fd678c9
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:37:08 +0530

    Update rotateArray.java

--- rotateArray.java
+++ rotateArray.java
@@ -2,8 +2,23 @@ package lecture1;
 
 public class rotateArray {
 
-	public rotateArray() {
-		// TODO Auto-generated constructor stub
-	}
-
+	int temp[]=new int[d];
+    
+        int k=0;
+        int count=0;
+        for(int i=0; i<d ; i++){
+            temp[k]=arr[i];
+            k++;
+        }
+    
+        for(int j=0 ;j<arr.length-d; j++){
+           arr[j]=arr[j+d];
+           count++;
+        }
+        k=0;
+        for(int x=count ; x<arr.length ;x++){
+            arr[x]=temp[k];
+            k++;
+        }
+    }
 }
