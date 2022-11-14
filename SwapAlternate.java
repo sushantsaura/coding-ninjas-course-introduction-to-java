54f437a000e1eff2b96428813c257396ccf31eff
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:20:09 +0530

    Update SwapAlternate.java

--- SwapAlternate.java
+++ SwapAlternate.java
@@ -1,5 +1,24 @@
 package lecture1;
 
 public class SwapAlternate {
-
+   public static void swapAlternate(int arr[]) {
+        if(arr.length%2==0){
+         int   n=arr.length;
+       
+       for(int i=0 ; i<n; i+=2){
+           int temp=arr[i];
+           arr[i]=arr[i+1];
+           arr[i+1]=temp;
+       }
+        }
+        else{
+            int i;
+            for( i=0 ; i<arr.length-1;i+=2){
+                int temp=arr[i];
+           arr[i]=arr[i+1];
+           arr[i+1]=temp;
+            }
+            arr[arr.length-1]=arr[i];
+        }
+    }
 }
