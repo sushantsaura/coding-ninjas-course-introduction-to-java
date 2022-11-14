659c0e4d124cee487e3bdf89b54860407d72d56d
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:31:50 +0530

    Update tripletSum.java

--- tripletSum.java
+++ tripletSum.java
@@ -1,5 +1,17 @@
 package lecture1;
 
 public class tripletSum {
+   int count=0;
+         for(int i=0 ; i<arr.length ; i++){
+             for( int j=i+1 ; j<arr.length ; j++){
+                 for( int k=j+1 ; k<arr.length ; k++){
+                     if(x==(arr[i]+arr[j]+arr[k])){
+                         count++;
+                     }
+                 }
+             }
+         }
+         return count;
+    }
 
 }
