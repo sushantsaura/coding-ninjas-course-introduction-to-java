bf8c3863e26252db736c37a8e30f90384059ef11
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:27:22 +0530

    Update Sort01.java

--- Sort01.java
+++ Sort01.java
@@ -1,5 +1,16 @@
 package lecture1;
 
 public class Sort01 {
-
+  int odd=1;
+        int even=2;
+    	for(int i=0;i<(n+1)/2;i++){
+            
+              arr[i]=odd;
+            odd+=2;
+        }
+        for(int i=0;i<n/2;i++){
+           arr[n-i-1]=even;
+            even+=2;
+        }
+    }
 }
