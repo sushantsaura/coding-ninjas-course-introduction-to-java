b34d05e9fadec85dff3eed94b1a328edd56e7a98
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:42:59 +0530

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -1,5 +1,19 @@
 package lecture1;
 
 public class binarySearch {
+    int start=0;
+       int end=arr.length-1;
+       while(start<=end){
+           int mid=(start+end)/2;
+           if(x>arr[mid]){
+               start=mid+1;
+           }else if(x==arr[mid]){
+               return mid;
+           }else{
+               end=mid-1;
+           }
+       }
+       return -1;
+    }
 
 }
