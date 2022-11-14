1fd6449bb2a11a72599cb5c709b2e13b477683e9
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:31:09 +0530

    Update pairSum.java

--- pairSum.java
+++ pairSum.java
@@ -1,5 +1,15 @@
 package lecture1;
 
 public class pairSum {
-
+ int count=0;
+       for(int i=0;i<arr.length;i++){
+           int k=x-arr[i];
+           for(int j=i+1;j<arr.length;j++){
+           if(arr[j]==k){
+                count++;
+           }
+           }
+       }
+       return count;
+    }
 }
