5484b5ccf1f698cdbbbac0581abd5887eb62c4a8
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:37:57 +0530

    Update pushZeroatEnd.java

--- pushZeroatEnd.java
+++ pushZeroatEnd.java
@@ -1,5 +1,20 @@
 package lecture1;
 
 public class pushZeroatEnd {
-
+         int len = arr.length;
+        int i = 0 ;
+        int k = 0;
+        int temp = 0;
+        
+        for(i = 0; i<len; i++){
+            if (arr[i]!=0){
+                
+                //swap elements
+                temp = arr[k];
+                arr[k] = arr[i];
+                arr[i] = temp;
+                k++;
+            }
+        }
+        }
 }
