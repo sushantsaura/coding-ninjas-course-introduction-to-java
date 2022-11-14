109e979cc35ad873749e436fcab7709561ed2912
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:39:00 +0530

    Update mergeSort.java

--- mergeSort.java
+++ mergeSort.java
@@ -1,5 +1,34 @@
 package lecture1;
 
 public class mergeSort {
+  int n=arr1.length+arr2.length;
+    	int temp[]=new int[n];
+        int i=0;
+        int j=0;
+        int k=0;
 
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<arr2[j]){
+               temp[k]=arr1[i];
+               k++;
+               i++;
+            }else{
+                temp[k]=arr2[j];
+                k++;
+                j++;
+            }
+        }
+         
+         while(i<arr1.length){
+                 temp[k]=arr1[i];
+               k++;
+               i++;
+         }
+         while(j<arr2.length){
+              temp[k]=arr2[j];
+                k++;
+                j++;
+         }
+        return temp; 
+    }
 }
