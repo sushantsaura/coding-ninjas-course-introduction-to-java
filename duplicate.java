da09ebda2cf62575d190ca04a40ee9c383a65277
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:29:24 +0530

    Update duplicate.java

--- duplicate.java
+++ duplicate.java
@@ -1,5 +1,21 @@
 package lecture1;
 
 public class duplicate {
-
+	int i,j;
+        int  flag=0;
+        for(i=0;i<arr.length;i++){
+            int x=arr[i];
+            flag=0;
+            for(j=0;j<arr.length;j++){
+                if(i!=j  && arr[j]==x){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1){
+                break;
+            }
+        }
+        return arr[i];
+    }
 }
