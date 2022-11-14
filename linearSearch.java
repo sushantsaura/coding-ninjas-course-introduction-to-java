730b020c2e405bd5f347d03d4ab30ddd86c02794
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:25:25 +0530

    Update linearSearch.java

--- linearSearch.java
+++ linearSearch.java
@@ -1,5 +1,22 @@
 package lecture1;
 
-public class linearSearch {
 
+  public static int linearSearch(int arr[], int x) {
+        
+        int t=0;
+	   for(int i=0;i<arr.length;i++){
+           if(arr[i]==x){
+               t=i;
+               break;
+           }else{
+               t=0;
+           }
+       }
+        if(t!=0){
+            return t;
+        }else{
+            return -1;
+        }
+       
+    }
 }
