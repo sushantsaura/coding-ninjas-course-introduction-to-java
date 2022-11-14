2f555d31dc31b05b145619b18575667f03e6854f
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:28:39 +0530

    Update findUnique.java

--- findUnique.java
+++ findUnique.java
@@ -1,5 +1,29 @@
 package lecture1;
 
 public class findUnique {
-
+  int flag=0;
+        int j;
+        int i;
+       
+       	for(i=0;i<arr.length;i++){
+          	
+            int x=arr[i];
+            flag = 0;
+            
+        	for(j=0;j<arr.length;j++){
+        		if(i!=j && arr[j]==x){
+              		flag=1;    
+              		break;
+                }              	
+            }
+            
+           	if(flag==0){
+               break;
+           	}
+    	}
+        
+        return arr[i];
+      
+      
+	}
 }
