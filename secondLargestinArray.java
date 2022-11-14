a999a52b43531983eec8fda2e54c37f2d4da78a0
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:36:24 +0530

    Update secondLargestinArray.java

--- secondLargestinArray.java
+++ secondLargestinArray.java
@@ -2,8 +2,28 @@ package lecture1;
 
 public class secondLargestinArray {
 
-	public secondLargestinArray() {
-		// TODO Auto-generated constructor stub
-	}
+	          int n=arr.length;
+          int largest=Integer.MIN_VALUE;
+          int second=Integer.MIN_VALUE-1;
+          int flag=0;
+          if(n<=1){
+              return Integer.MIN_VALUE;
+          }else{
+          for(int i=0 ; i<n ; i++){
+            
+              if(arr[i]>largest){
+                  second=largest;
+                  largest=arr[i];
+                 
+              }
+              else if(second< arr[i] && arr[i]  != largest){
+                  second = arr[i];
+              }
+              
+          }
+          return second;
+          }
+    
+    }
 
 }
