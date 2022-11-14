275afebd20aa59df3f872aa787001d408ffaa776
sushantsaura <91001647+sushantsaura@users.noreply.github.com>
2022-11-14 18:41:53 +0530

    Update selectionSort.java

--- selectionSort.java
+++ selectionSort.java
@@ -1,5 +1,19 @@
 package lecture1;
 
 public class selectionSort {
-
+  for(int i=0 ; i<arr.length ; i++){
+               int smallest=arr[i];
+               int minIndex=i;  
+               for(int j=i+1 ; j<arr.length ; j++){
+                   if(arr[j]<smallest){
+                     smallest=arr[j] ;
+                     minIndex=j;
+                   }
+               }
+             arr[minIndex]=arr[i];
+               arr[i]=smallest ;
+                
+           } 
+           
+    }   
 }
